aa1c410e0cd830ba48cf5736c1b9d2f1fbc62162
Hiroshi Uesugi <info@step-by.work>
2022-06-18 17:20:11 +0900

    no message

--- dist/_assets/js/main.js
+++ dist/_assets/js/main.js
@@ -1,43 +1,40 @@
 //ua判定
 document.addEventListener('DOMContentLoaded', function () {
-
-	const members = new Swiper('.js-members', {
-		speed: 400,
-		spaceBetween: 0,
+	const trainerTab = new Swiper('.js-trainerTab', {
+		slidesPerView: 1,
 		effect: 'fade',
-		allowTouchMove: false,
 		fadeEffect: {
 			crossFade: true
 		},
-	});
-	$('.js-membersBtn button').click(function () {
-		$('.js-membersBtn button').removeClass('is-active')
+		allowTouchMove: false,
+	})
+	$('.js-trainerTabBtn button').click(function () {
+		$('.js-trainerTabBtn button').removeClass('is-active')
 		$(this).addClass('is-active')
 		let target = $(this).data('target')
-		members.slideTo(target)
-		return false;
+		trainerTab.slideTo(target);
 	})
 
-	const studio = new Swiper('.js-studio', {
-		speed: 400,
-		spaceBetween: 0,
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets',
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
+	const studioSlider = new Swiper('.js-studioSlider', {
+		slidesPerView: 1,
+		spaceBetween: 15,
 		autoplay: {
 			delay: 5000,
 		},
 		loop: true,
-	});
-
-	$('.js-faq dt').click(function () {
-		$(this).toggleClass('is-open');
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+		},
+	})
+	$('.js-faq').click(function () {
+		$(this).toggleClass('is-open')
 		$(this).next('dd').stop().slideToggle();
+		return false;
 	})
 })
 window.addEventListener('load', function () {
@@ -47,6 +44,5 @@ window.addEventListener('scroll', function () {
 		$('.l-hdr').addClass('is-scroll')
 	} else {
 		$('.l-hdr').removeClass('is-scroll')
-
 	}
 });
